db2408efbbd628ff3650f4e6b875f373cf4a166e
Slime Enjoyer <austinlanyon@gmail.com>
2023-06-07 16:39:21 +1000

    added some very basic scripts and major sprites

---
+++ Hang Man/Assets/Scripts/HangMan.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class HangMan : MonoBehaviour
+{
+    public int playerLives = 10;
+
+    public float wordChosen = Random.Range(1, 10);
+
+    public int letterCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
