ec4c6ae156cc12d127c6a3ac45e82975a8f4726a
Slime Enjoyer <austinlanyon@gmail.com>
2023-06-08 16:30:06 +1000

    added the code for the animation and live system as well as what word is active and how many letters are there. it going good rn

--- Hang Man/Assets/Scripts/HangMan.cs
+++ Hang Man/Assets/Scripts/HangMan.cs
@@ -7,19 +7,151 @@ public class HangMan : MonoBehaviour
 {
     public int playerLives = 10;
 
-    public float wordChosen = Random.Range(1, 10);
+    public float wordChosen = 1f; //Random.Range(1, 10);
 
     public int letterCount = 0;
+
+    public string chosenWord = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        wordChosen = Random.Range(1, 10);
+        Debug.Log(wordChosen);
+        //Code didn't work as intended initially
+
+        /*if (wordChosen == 1)
+        {
+            chosenWord = "Tempt";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 2)
+        {
+            chosenWord = "Glory";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 3)
+        {
+            chosenWord = "Radical";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 4)
+        {
+            chosenWord = "Pocket";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 5)
+        {
+            chosenWord = "Cart";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 6)
+        {
+            chosenWord = "Respect";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 7)
+        {
+            chosenWord = "Proposal";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 8)
+        {
+            chosenWord = "Return";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 9)
+        {
+            chosenWord = "Fog";
+            Debug.Log(chosenWord);
+        }
+        else if (wordChosen == 10)
+        {
+            chosenWord = "Wisecrack";
+            Debug.Log(chosenWord);
+        }*/
+    }
+
+    public void ClickButton(int buttonClicked)
+    {
+        if (buttonClicked == 1)
+        playerLives--;
+        else if (buttonClicked == 2)
+            playerLives++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (wordChosen == 1)
+        {
+            chosenWord = "Tempt";
+            Debug.Log(chosenWord);
+            letterCount = 5;
+        }
+        else if (wordChosen == 2)
+        {
+            chosenWord = "Glory";
+            Debug.Log(chosenWord);
+            letterCount = 5;
+        }
+        else if (wordChosen == 3)
+        {
+            chosenWord = "Radical";
+            Debug.Log(chosenWord);
+            letterCount = 7;
+        }
+        else if (wordChosen == 4)
+        {
+            chosenWord = "Pocket";
+            Debug.Log(chosenWord);
+            letterCount = 6;
+        }
+        else if (wordChosen == 5)
+        {
+            chosenWord = "Cart";
+            Debug.Log(chosenWord);
+            letterCount = 4;
+        }
+        else if (wordChosen == 6)
+        {
+            chosenWord = "Respect";
+            Debug.Log(chosenWord);
+            letterCount = 7;
+        }
+        else if (wordChosen == 7)
+        {
+            chosenWord = "Proposal";
+            Debug.Log(chosenWord);
+            letterCount = 8;
+        }
+        else if (wordChosen == 8)
+        {
+            chosenWord = "Return";
+            Debug.Log(chosenWord);
+            letterCount = 6;
+        }
+        else if (wordChosen == 9)
+        {
+            chosenWord = "Fog";
+            Debug.Log(chosenWord);
+            letterCount = 3;
+        }
+        else if (wordChosen == 10)
+        {
+            chosenWord = "Wisecrack";
+            Debug.Log(chosenWord);
+            letterCount = 9;
+        }
+
+        if(playerLives >= 10)
+        {
+            playerLives = 10;
+        }
+
+        if (playerLives <= 0)
+        {
+            playerLives = 0;
+        }
     }
 }
---
+++ Hang Man/Assets/Scripts/SpriteChange.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SpriteChange : MonoBehaviour
+{
+    public GameObject hangMan;
+
+    public Sprite hangBase;
+    public Sprite hang01;
+    public Sprite hang02;
+    public Sprite hang03;
+    public Sprite hang04;
+    public Sprite hang05;
+    public Sprite hang06;
+    public Sprite hang07;
+    public Sprite hang08;
+    public Sprite hang09;
+    public Sprite hang10;
+
+    public SpriteRenderer spriteRenderer;
+
+    private int lives = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives = hangMan.GetComponent<HangMan>().playerLives;
+        if (lives == 10)
+        {
+            spriteRenderer.sprite = hangBase;
+        }
+        else if (lives == 9)
+        {
+            spriteRenderer.sprite = hang01;
+        }
+        else if (lives == 8)
+        {
+            spriteRenderer.sprite = hang02;
+        }
+        else if (lives == 7)
+        {
+            spriteRenderer.sprite = hang03;
+        }
+        else if (lives == 6)
+        {
+            spriteRenderer.sprite = hang04;
+        }
+        else if (lives == 5)
+        {
+            spriteRenderer.sprite = hang05;
+        }
+        else if (lives == 4)
+        {
+            spriteRenderer.sprite = hang06;
+        }
+        else if (lives == 3)
+        {
+            spriteRenderer.sprite = hang07;
+        }
+        else if (lives == 2)
+        {
+            spriteRenderer.sprite = hang08;
+        }
+        else if (lives == 1)
+        {
+            spriteRenderer.sprite = hang09;
+        }
+        else if (lives == 0)
+        {
+            spriteRenderer.sprite = hang10;
+        }
+    }
+}
